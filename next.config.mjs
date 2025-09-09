5667fe2933d5b9f2df127898110c0e4de6d21266
MichaelMPV <cpaucarv@uni.pe>
2025-09-09 08:49:10 +0200

    Static export con Next + Amplify

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,8 @@ const nextConfig = {
   // Activamos react strict mode para ayudar a detectar problemas
   reactStrictMode: true,
   // Desactivamos la optimización de imágenes para poder servir nuestros
+  // Muy importante: genera HTML estático listo para CDN (export)
+  output: 'export',
   // recursos locales sin pasar por el servidor de imágenes de Next.js.
   images: {
     unoptimized: true,
