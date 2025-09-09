bef96d299d946930c4a583dcae23464cf3a87683
MichaelMPV <cpaucarv@uni.pe>
2025-09-09 10:05:02 +0200

    testimonio

--- app/page.tsx
+++ app/page.tsx
@@ -32,6 +32,7 @@ export default function Home() {
       <QuoteSection />
       {/* <Service /> */} 
       <Gallery />
+      <Testimonials />
       <Contact />
       <Testimonials />
       <Footer />
